df0ea0a1d004528f19274fac127fb38875cc084b
Hervé EVODA <herveevoda@gmail.com>
2022-02-05 14:16:31 +0100

    Adding Authors view and Thymeleaf

---
+++ spring5app/src/main/java/herve/springframework/spring5app/controllers/AuthorController.java
@@ -0,0 +1,25 @@
+package herve.springframework.spring5app.controllers;
+
+import herve.springframework.spring5app.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors" , authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
